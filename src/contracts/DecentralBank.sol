6d50681fbdc8376b749f6ea1e401454690aab3c7
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2022-04-23 06:33:59 +0100

    defi done

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -10,8 +10,55 @@ contract DecentralBank {
     Tether public tether;
     RWD public rwd;
 
+    address[] public stakers;
+
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(RWD _rwd, Tether _tether) public {
         rwd = _rwd;
         tether = _tether;
+        owner = msg.sender;
+    }
+
+    function depositTokens(uint _amount) public {
+        require(_amount > 0, 'amount cannot be 0');
+        tether.transferFrom(msg.sender, address(this), _amount);
+
+        //Update Staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        if(!hasStaked[msg.sender]){
+            stakers.push(msg.sender);
+        }
+        
+        //update staking balance
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    function unstakeTokens() public {
+        uint balance = stakingBalance[msg.sender];
+        require(balance > 0, "staking balance cannot be less than zero");
+
+        tether.transfer(msg.sender, balance);
+
+        stakingBalance[msg.sender] = 0;
+        isStaking[msg.sender] = false;
+
+
+    }
+
+    function issueTokens() public {
+      require(msg.sender == owner, 'caller must be the owner');
+       for(uint i=0; i<stakers.length; i++){
+           address recipient = stakers[i];
+           uint balance = stakingBalance[recipient] / 9;
+           if(balance > 0){
+             rwd.transfer(recipient, balance);
+           }
+
+       }
     }
 } 
--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract Tether {
     string public name = 'Mock Tether Token';
-    string public symbol = 'USDT';
+    string public symbol = 'mUSDT';
     uint256 public totalSupply = 1000000000000000000000000; // 1 million tokens
     uint8 public decimals = 18;
 
